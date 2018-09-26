ce07fc449f8c05baa1f0e468ac1cd4028cb00372
badriaila <36105479+badriaila@users.noreply.github.com>
2018-09-26 11:03:01 +0530

    added dashboard content

--- src/app/dashboard-content/dashboard-content.component.ts
+++ src/app/dashboard-content/dashboard-content.component.ts
@@ -9,6 +9,19 @@ export class DashboardContentComponent implements OnInit {
 
   constructor() { }
 
+  totalCount = [
+    {city:'NRT',data:"10",routeId:'01'},
+    {city:'GNT',data:"10",routeId:'02'},
+    {city:'SAP',data:"10"},
+    {city:'VNK',data:"10"},
+    {city:'PDRL',data:"10"},
+    {city:'CPT',data:"10"},
+    {city:'MCL',data:"10"},
+    {city:'MTR',data:"10"},
+    {city:'Total',data:"10"},
+
+  ];
+
   ngOnInit() {
   }
 
